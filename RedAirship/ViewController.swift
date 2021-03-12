db5ad00058bb39c8ef50a0f166d52ae1985275cd
Ky Nguyen <nguyentruongky33@gmail.com>
2021-03-12 17:54:04 +0700

    Initial Commit

---
+++ RedAirship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedAirship
+//
+//  Created by Ky Nguyen on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
